d23823a783a178f1a91a872359bb512e068d3863
Madiha <RGAM9L>
2022-03-08 14:37:17 +0000

    thirdrepoi

---
+++ third report/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace third_report
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(  "hello world form main line");
+        }
+    }
+}
